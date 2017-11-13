de64ea8fb219b946e7a11959d795ddbb7e93ae63
Rami Taibah <taibah@gmail.com>
2017-11-13 18:27:56 +0300

    Implement Persistence

--- App.js
+++ App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 
 import {Provider} from 'react-redux';
-import {createStore, compose, applyMiddleWare} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import reducers from './src/reducers';
+
+import {persistStore, autoRehydrate} from 'redux-persist';
+import {AsyncStorage} from 'react-native';
 
 import {StackNavigator} from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
@@ -11,7 +14,16 @@ import DeckHomeScreen from './src/screens/DeckHomeScreen';
 import AddDeckScreen from './src/screens/AddDeckScreen';
 import QuizScreen from './src/screens/QuizScreen.js';
 
-const store = createStore(reducers, {}, compose());
+const store = createStore(
+  reducers,
+  {},
+  compose(applyMiddleware(), autoRehydrate()),
+);
+
+persistStore(store, {
+  storage: AsyncStorage,
+  whitelist: ['quiz', 'deck'],
+}).purge();
 
 export default class Home extends React.Component {
   render() {
